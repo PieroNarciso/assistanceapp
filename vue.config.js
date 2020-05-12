cfdcfd6f2bb36b860ed4820612e16731aae24a3c
Piero Narciso <pieronarciso988@gmail.com>
2020-05-11 20:22:57 -0500

    msTileColor

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
   pwa: {
     name: 'Assistance App',
     themeColor: '#03A9F4',
+    msTileColor: '#FAFAFA',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
   }
