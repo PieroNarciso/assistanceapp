1fbafe2c8f2b9a3bcd0f3ca486bebe8688eb005a
Piero Narciso <pieronarciso988@gmail.com>
2020-05-30 21:54:30 -0500

    Meta tags, and lazy download

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,15 +19,12 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
   },
   {
     path: '/user',
     name: 'User',
-    component: ViewUser,
+    component: () => import('../views/ViewUser.vue'),
     meta: {
       title: 'Assistance | User'
     }
@@ -36,7 +33,7 @@ const routes = [
   {
     path: '/code-view',
     name: 'CodeView',
-    component: ViewCode,
+    component: () => import('../views/ViewCode.vue'),
     meta: {
       title: 'Code View'
     }
